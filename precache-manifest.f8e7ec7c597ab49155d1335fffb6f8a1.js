c96727772c4f692adcc21c92f73ab6cba96cbcc6
morris754 <morris754@gmail.com>
2020-01-30 23:12:46 +1100

    Updates

---
+++ precache-manifest.f8e7ec7c597ab49155d1335fffb6f8a1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a04fd851dfcd0b41b744af0b857baa7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "fa3795dedb0a34f6240a",
+    "url": "/portfolio/static/css/2.a641e29a.chunk.css"
+  },
+  {
+    "revision": "95a891e70fd3b8f2031d",
+    "url": "/portfolio/static/css/main.5782237a.chunk.css"
+  },
+  {
+    "revision": "fa3795dedb0a34f6240a",
+    "url": "/portfolio/static/js/2.d8bff08d.chunk.js"
+  },
+  {
+    "revision": "8a14dcea7ad8b593c1e83c11f7ba1a89",
+    "url": "/portfolio/static/js/2.d8bff08d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "95a891e70fd3b8f2031d",
+    "url": "/portfolio/static/js/main.2746c0d9.chunk.js"
+  },
+  {
+    "revision": "1f3bb0d4088d0d8d0107",
+    "url": "/portfolio/static/js/runtime-main.cf6b54d3.js"
+  },
+  {
+    "revision": "eeb8984d407a5db14792b2a5a50b843b",
+    "url": "/portfolio/static/media/HDS.eeb8984d.png"
+  },
+  {
+    "revision": "faacb75e81ed335e025b9e95a356429a",
+    "url": "/portfolio/static/media/aetna.faacb75e.jpeg"
+  },
+  {
+    "revision": "18f2d327ebd4784d24c3d48f6a6489f7",
+    "url": "/portfolio/static/media/family.18f2d327.png"
+  },
+  {
+    "revision": "dcb08a45635ff39bdb31cbb13997a79b",
+    "url": "/portfolio/static/media/friends.dcb08a45.png"
+  },
+  {
+    "revision": "a9a01fc9ff848bca892cb346955e86c6",
+    "url": "/portfolio/static/media/milkjob.a9a01fc9.jpg"
+  },
+  {
+    "revision": "5834387eaae5f1b668e4e04c25e9f211",
+    "url": "/portfolio/static/media/mount2.5834387e.jpg"
+  },
+  {
+    "revision": "62be491889528186e5d13283179af040",
+    "url": "/portfolio/static/media/pap.62be4918.bmp"
+  },
+  {
+    "revision": "229befd7594251b167437cfa9bbcd7d2",
+    "url": "/portfolio/static/media/profile.229befd7.png"
+  },
+  {
+    "revision": "ebf95bf8b1afcda35185a72de4d9c500",
+    "url": "/portfolio/static/media/slither.ebf95bf8.gif"
+  },
+  {
+    "revision": "f7dc198acb21aa0e9ed2e8f6f7f2c8de",
+    "url": "/portfolio/static/media/smp.f7dc198a.jpg"
+  }
+]);

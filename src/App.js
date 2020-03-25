f8895fc599686944747d7b0d08f32aa47d200825
morris754 <morris754@gmail.com>
2020-03-24 22:14:16 -0400

    hash router

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Home from './components/Home';
 import projectList from './components/projectList'
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route,
 } from "react-router-dom";

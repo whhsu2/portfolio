c3d61be76fd71ab2b81c340bab276f0c23586381
morris754 <morris754@gmail.com>
2020-10-05 18:56:24 -0400

    change bio

--- src/components/About.js
+++ src/components/About.js
@@ -28,9 +28,9 @@ const About = () => {
                 </div> 
             </div>
             <div className="me">
-                <p>I just graduated from Harvard Health Data Science program and currently am working at Aetna as a Data Engineer. Previously 
+                <p>I just graduated from Harvard Health Data Science program and currently am working at Aetna as a Software Engineer. Previously 
                 I studied Nano Science at NCTU in Taiwan, mainly focusing on semiconductor fabrication.
-                Since then, I have taken a huge pivot, now I am working on digitizing the healthcare experience. 
+                Since then, I have taken a huge pivot, now I am a software developer that focus on digitising the healthcare experience. 
                 </p>
                 <p>
                 In leasure time, I write about random stuff that I'm interested in (<a href="https://medium.com/@morrishsu_94479" style={{fontSize: "15px"}}>my blog posts</a>).  I write some full stack and front end projects. Besides coding, I started to learn Piano and reading sheet music since the quarantine.
